bf44c9fa6c85203e652847382f9b7413e5216a05
hazal köksal <hazal-koksal@windowslive.com>
2021-04-21 17:51:49 +0300

    ...

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("getbymail")]
+        public ActionResult GetByMail(string email)
+        {
+            var result = _userService.GetByMail(email);
+
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
